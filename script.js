56cf6646fadd30212d843fb685a1a2a4c4e6f937
safinxr <safinxr@gmail.com>
2024-02-09 05:19:26 +0600

    project done

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const allCards = document.querySelectorAll("cards-scroll,  ");
-console.log(allCards);
+const allCards = document.querySelectorAll(".card");
+const headerHeight =70;
+const baseWidth = 60;
+if(allCards.length){
+    allCards.forEach((card, i)=>{
+        console.log(card);
+        const incValue = i*headerHeight;
+        const decValue =(allCards.length -i )* headerHeight;
+        const widthValue = (allCards.length -i )* baseWidth;
+
+        card.setAttribute("style", `top: ${incValue}px; margin-bottom: ${decValue}px; margin-top: ${incValue}px; bottom: calc(-90vh + ${decValue}px);`)
+    })
+}
